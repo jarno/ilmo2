9d1acd638b2bd64e96d966e14cf4e87506397bb8
Jarno Kyykkä <jkyykka@cs.helsinki.fi>
2009-11-10 00:15:18 +0200

    seeds kuntoon

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+
+namespace :options do
+	if ENV['count']
+		@count = ENV['count'].to_i
+		if @count > 1000
+			@count = 1000
+		end
+	end
+	if ENV['mode'] == "delete"
+		Course.delete_all
+	end
+end
+
+index = 1
+if Course.count > 0
+	a = Course.find(:last)
+	index = a.id
+end	
+	
+@count.times do 
+	Course.create(:name => "Course #{index}", :description => "diipaapa #{index}")
+	index = index+1	
+end			
